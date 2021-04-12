48ee1ccfc7fed895c41072f1bd270641a59e0a7d
Thomas van der Westen <thomas@pixelpillow.nl>
2021-04-12 14:33:02 +0200

    Upgrade NPM deps

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,50 +1,50 @@
-const amply = require('./../src/main');
-const assert = require('assert');
+const amply = require("./../src/main");
+const assert = require("assert");
 
-describe('The main module', function () {
-	describe('The function getCharts', function () {
-		it('Resolves the promise with a count high then 0', function () {
+describe("The main module", function () {
+	describe("The function getCharts", function () {
+		it("Resolves the promise with a count high then 0", function () {
 			//Act
 			return amply.getCharts().then((result) => {
 				assert.notStrictEqual(
 					result.count,
 					0,
-					'The result count is not zero.'
+					"The result count is not zero."
 				);
 			});
 		});
 
-		it('Resolves the promise with a list item count high then 0', function () {
+		it("Resolves the promise with a list item count high then 0", function () {
 			//Act
 			return amply.getCharts().then((result) => {
 				assert.notStrictEqual(
 					result.list.length,
 					0,
-					'The result list items count is not zero.'
+					"The result list items count is not zero."
 				);
 			});
 		});
 
-		it('Resolves the promise with a expected count of objects', function () {
+		it("Resolves the promise with a expected count of objects", function () {
 			//Act
 			return amply.getCharts().then((result) => {
 				assert.strictEqual(
 					result.count,
 					200,
-					'The result count is correct.'
+					"The result count is correct."
 				);
 			});
 		});
 
-		it('Resolves the promise with a list of item with the expexted keys', function () {
+		it("Resolves the promise with a list of item with the expexted keys", function () {
 			//Act
 			return amply.getCharts().then((result) => {
 				assert(Object.keys(result.list[0]), [
-					'place',
-					'title',
-					'artist',
-					'streams',
-					'url',
+					"place",
+					"title",
+					"artist",
+					"streams",
+					"url",
 				]);
 			});
 		});
